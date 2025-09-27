f8f31a7cb1dde5a185bf80361352de1da8dfbd3f
zxiaosi <2692159310@qq.com>
2025-09-27 22:35:54 +0800

    fix(packages): 初始化主子应用项目

---
+++ packages/main/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>主应用main</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ packages/subapp1/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>子应用subapp1</h1>
+    </>
+  );
+}
+
+export default App;
