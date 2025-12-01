d06da44ff52b9029ecd24fc77fabedcd97d01b53
zxiaosi <2692159310@qq.com>
2025-12-01 22:43:56 +0800

    feat(packages): subapp1 应用配置 qiankun 信息

--- packages/main/src/pages/Layout.tsx
+++ packages/main/src/pages/Layout.tsx
@@ -1,15 +1,25 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 /** 布局组件 */
 const Layout = () => {
+  const navigate = useNavigate();
+
+  const handlePageTo = (uri: string) => {
+    navigate(uri);
+  };
+
   return (
     <div className="layout">
       <h1>布局组件</h1>
 
       <div style={{ display: 'flex', gap: 20 }}>
-        <button>主应用 - Home</button>
-        <button>子应用 - subapp1</button>
-        <button>子应用 - subapp2</button>
+        <button onClick={() => handlePageTo('/home')}>主应用 - Home</button>
+        <button onClick={() => handlePageTo('/subapp1')}>
+          子应用 - subapp1
+        </button>
+        <button onClick={() => handlePageTo('/subapp2')}>
+          子应用 - subapp2
+        </button>
       </div>
 
       <Outlet />
--- packages/subapp1/src/main.tsx
+++ packages/subapp1/src/main.tsx
@@ -1,13 +1,39 @@
 import { sdk } from '@zxiaosi/sdk';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { createRoot, type Root } from 'react-dom/client';
+import { name } from '../package.json';
 import App from './App.tsx';
 import './index.css';
 
-sdk.extend('sdk');
+let root: Root;
+const render = (props: any = {}) => {
+  const container = props?.container
+    ? props.container.querySelector('#root')
+    : document.getElementById('root');
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+  root = createRoot(container);
+
+  root.render(<App />);
+};
+
+if (!window.__POWERED_BY_QIANKUN__) {
+  render();
+}
+
+export async function bootstrap() {
+  console.log(`${name} bootstrap`);
+}
+
+export async function mount(props: any) {
+  console.log(`${name} mount`, props);
+  sdk.extend('sdk');
+  render(props);
+}
+
+export async function unmount(props: any) {
+  console.log(`${name} unmount`, props);
+  root.unmount();
+}
+
+export async function update(props: any) {
+  console.log(`${name} update`, props);
+}
