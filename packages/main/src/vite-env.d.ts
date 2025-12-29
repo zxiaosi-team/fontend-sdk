31cdd326cbe7eca51096955aab9b43ddbbb295d8
zxiaosi <2692159310@qq.com>
2025-12-29 23:12:57 +0800

    feat(packages): 添加 env 环境变量

--- packages/main/src/vite-env.d.ts
+++ packages/main/src/vite-env.d.ts
@@ -1,6 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_PORT: string;
   readonly VITE_REACT_JS_PATH: string;
   readonly VITE_REACT_DOM_JS_PATH: string;
 }
--- packages/subapp1/vite.config.ts
+++ packages/subapp1/vite.config.ts
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-import { ConfigEnv, defineConfig } from 'vite';
+import { ConfigEnv, defineConfig, loadEnv } from 'vite';
 import { viteExternalsPlugin } from 'vite-plugin-externals';
 import qiankun from 'vite-plugin-qiankun-lite';
 
@@ -10,11 +10,16 @@ import { name } from './package.json';
 export default ({ mode }: ConfigEnv) => {
   // 是否生产环境
   const isProduction = mode === 'production';
+  // 环境变量文件夹
+  const envDir = resolve(__dirname, 'env');
+  // 加载环境变量
+  const env = loadEnv(mode, envDir);
 
   return defineConfig({
     base: '/',
+    envDir: envDir,
     server: {
-      port: 5174, // 服务端口
+      port: Number(env.VITE_PORT), // 服务端口
     },
     resolve: {
       alias: {
