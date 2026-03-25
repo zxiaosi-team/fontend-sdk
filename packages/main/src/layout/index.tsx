5ec7eee95ba7a0a7298894d61a3f53aa6ba99e7d
zxiaosi <2692159310@qq.com>
2026-03-26 01:01:43 +0800

    refactor(packages): 重构布局组件结构

---
+++ packages/main/src/layout/index.tsx
@@ -0,0 +1,75 @@
+import ProLayout from '@ant-design/pro-layout';
+import { sdk } from '@zxiaosi/sdk';
+import { Suspense } from 'react';
+import { Outlet, useLocation, useMatches, useNavigate } from 'react-router-dom';
+import { useStore } from 'zustand';
+
+import dynamicIcon from '@/components/dynamicIcon';
+
+import CustomActions from './customActions';
+
+/** 布局组件 */
+const Layout: React.FC = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const matches = useMatches();
+
+  const locale = useStore(sdk.store, (state) => state.locale);
+
+  const currentMatch = matches[matches.length - 1]?.handle?.crumb() || {};
+  const noLayout = JSON.parse(currentMatch?.routeAttr || '{}')?.noLayout;
+
+  /** 菜单点击事件 */
+  const handleMenuClick = (item: any) => {
+    navigate(item.path);
+  };
+
+  /** 菜单头点击事件 */
+  const handleMenuHeaderClick = () => {
+    navigate('/');
+  };
+
+  /** 页面切换事件 */
+  const handlePageChange = (location: Location) => {
+    // 是否有用户信息
+    if (!sdk.app.user || Object.keys(sdk.app.user).length === 0)
+      return sdk.app.pageToLogin();
+  };
+
+  return (
+    <ProLayout
+      title='小四先生的栈'
+      layout='mix'
+      locale={locale as any}
+      formatMessage={({ id, defaultMessage }: any) =>
+        sdk.i18n.intl.get(id).d(defaultMessage)
+      }
+      location={location}
+      menuItemRender={(item, dom) => (
+        <div onClick={() => handleMenuClick(item)}>{dom}</div>
+      )}
+      onMenuHeaderClick={handleMenuHeaderClick}
+      onPageChange={handlePageChange}
+      {...(noLayout && {
+        headerRender: false,
+        footerRender: false,
+        menuRender: false,
+      })}
+      menu={{
+        request: async () => sdk.app.menuData || [],
+      }}
+      actionsRender={(props) => <CustomActions {...props} />}
+      menuDataRender={(menuData) => {
+        return menuData.map((_: any) => ({ ..._, icon: dynamicIcon(_?.icon) }));
+      }}
+    >
+      <Suspense
+        fallback={sdk.ui.renderComponent('Loading', { isSuspense: true })}
+      >
+        <Outlet />
+      </Suspense>
+    </ProLayout>
+  );
+};
+
+export default Layout;
