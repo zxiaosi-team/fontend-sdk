f28154017edf890152c30676cfb04a922f7dcda3
zxiaosi <2692159310@qq.com>
2026-06-01 13:24:21 +0800

    breaking(sdk): SdkApiPlugin插件重构，默认 fetch 请求

--- packages/main/src/service/index.ts
+++ packages/main/src/service/index.ts
@@ -2,15 +2,27 @@ import { sdk } from '@zxiaosi/sdk';
 
 /** 获取用户信息 */
 export const getUserInfoApi = async () => {
-  return sdk.api.request('/getUserInfo', { method: 'GET' });
+  const token = sdk.storage.getToken();
+
+  const resp = await sdk.api.request('/api/getUserInfo', {
+    method: 'GET',
+    headers: { Authorization: token, 'content-type': 'application/json' },
+  });
+  console.log('getUserInfoApi resp', resp);
+  return resp;
 };
 
 /** 获取路由 */
 export const getRoutesApi = async () => {
-  return sdk.api.request('/getRoutes', { method: 'GET' });
+  const token = sdk.storage.getToken();
+
+  return sdk.api.request('/api/getRoutes', {
+    method: 'GET',
+    headers: { Authorization: token, 'content-type': 'application/json' },
+  });
 };
 
 /** 登录接口 */
 export const loginApi = async (params: any) => {
-  return sdk.api.request('/login', { method: 'POST', data: params });
+  return sdk.api.request('/api/login', { method: 'POST', body: params });
 };
