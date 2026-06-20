1eca46168963e260d8f8c7259696ab5c56b6b851
zxiaosi <2692159310@qq.com>
2026-06-21 02:10:59 +0800

    fix(sdk): 更新插件类型定义以支持泛型

--- packages/main/src/sdk.config.ts
+++ packages/main/src/sdk.config.ts
@@ -17,7 +17,6 @@ import CustomCrumb from '@/components/customCrumb/index.tsx';
 import CustomWithAuth from '@/components/customWithAuth/index.tsx';
 import I18nConfig from '@/i18n/index';
 import { request } from '@/request';
-import { getRoutesApi, getUserInfoApi, loginApi } from '@/service';
 
 import type { TestStoreProps } from './vite-env';
 
@@ -36,11 +35,8 @@ const createTestSlice: StoreSlice<TestStoreProps> = (set, get) => ({
 // 类型提示在 vite-env.d.ts 中
 sdk
   .use(SDKApiPlugin, {
-    controllers: new Map(), // 请求控制器
-    request, // 适配请求类
-    getRoutesApi,
-    getUserInfoApi,
-    loginApi,
+    controllers: new Map(),
+    request,
   })
   .use(SDKAppPlugin, {
     loadMicroApp,
